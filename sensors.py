dbd65f9eb6916c9d1e2bf6a3819d9893f17d41bf
Chandler Newman <chandler2newman@hotmail.co.uk>
2018-07-27 14:32:54 +0100

    Update sensors.py

--- sensors.py
+++ sensors.py
@@ -7,11 +7,7 @@ class SensorsMon(AgentCheck):
         sensors.init()
         try:
             for chip in sensors.iter_detected_chips():
-                #print '%s at %s' % (chip, chip.adapter_name)
                 for feature in chip:
-                    #print '  %s: %.2f' % (feature.label, feature.get_value())   
-                    #try:
                     self.gauge("sensors." + feature.label, feature.get_value(), device_name=('%s' % (chip)))
-                    #except KeyError:
         finally:
             sensors.cleanup()
